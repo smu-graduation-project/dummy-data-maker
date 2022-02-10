418ce1cdbd329b495ee0f989dd922a20d9138dd5
Jeeseob <jeeseob5761@gmail.com>
2022-02-10 16:03:31 +0900

    dummy data 초기

---
+++ src/test/java/com/smugraduationproject/dummyDataMaker/DummyDataMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smugraduationproject.dummyDataMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyDataMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
